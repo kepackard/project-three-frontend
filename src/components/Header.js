98792846071799a0866df661ec82c371195b6174
kepackard <kepackard@gmail.com>
2021-10-28 21:44:02 -0400

    Adds router and nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Header(props) {
-    return <h1>Header Name</h1>;
+
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <div>National Park Lesson Plans</div>
+            </Link>
+        </nav>
+    );
   }
   
   export default Header;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index.js";
+import Show from "../pages/Show.js";
+
 function Main(props) {
-    return <h1>Main Page</h1>;
+
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index />
+                </Route>
+                <Route path="/plans/:id" render={(rp) => <Show {...rp} />} />
+            </Switch>
+        </main>
+    );
   }
   
   export default Main;
